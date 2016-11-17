a16122e70a05c940e09403bf8ae605475a022291
Doncho Minkov <DonchoMinkov@gmail.com>
2016-11-17 14:38:57 +0200

    Added .eslintrc.js file

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 /* globals console require setTimeout Promise */
-"use strict";
-
-const os = require("os");
-
-const genres = ["action", "sci-fi", "fantasy", "horror", "comedy"];
-const pagesCount = 2;
+'use strict';
 
 const httpRequester = require("./utils/http-requester");
 const htmlParser = require("./utils/html-parser");
-
 const queuesFactory = require("./data-structures/queue");
+const modelsFactory = require("./models");
+const constants = require("./config/constants");
+
+require("./config/mongoose")(constants.connectionString);
 
 let urlsQueue = queuesFactory.getQueue();
 
 function wait(time) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve();
         }, time);
     });
 }
 
-genres.forEach(genre => {
-    for (let i = 0; i < pagesCount; i += 1) {
+constants.genres.forEach(genre => {
+    for (let i = 0; i < constants.pagesCount; i += 1) {
         let url = `http://www.imdb.com/search/title?genres=${genre}&title_type=feature&0sort=moviemeter,asc&page=${i+1}&view=simple&ref_=adv_nxt`;
         urlsQueue.push(url);
     }
@@ -37,8 +35,13 @@ function getMoviesFromUrl(url) {
             return htmlParser.parseSimpleMovie(selector, html);
         })
         .then(movies => {
-            console.log(movies.length);
-            return wait(500);
+            let dbMovies = movies.map(movie => {
+                return modelsFactory.getSimpleMovie(movie.title, movie.url);
+            });
+
+            modelsFactory.insertManySimpleMovies(dbMovies);
+
+            return wait(1000);
         })
         .then(() => {
             if (urlsQueue.isEmpty()) {
@@ -52,7 +55,7 @@ function getMoviesFromUrl(url) {
         });
 }
 
-const asyncPagesCount = 5;
+const asyncPagesCount = 15;
 
 Array.from({ length: asyncPagesCount })
-    .forEach(_ => getMoviesFromUrl(urlsQueue.pop()));
+    .forEach(() => getMoviesFromUrl(urlsQueue.pop()));
---
+++ models/index.js
@@ -0,0 +1,12 @@
+/* globals module require */
+
+const SimpleMovie = require("./simple-movie-model");
+
+module.exports = {
+    getSimpleMovie(name, url) {
+        return SimpleMovie.getSimpleMovieByNameAndUrl(name, url);
+    },
+    insertManySimpleMovies(movies) {
+        SimpleMovie.insertMany(movies);
+    }
+};
