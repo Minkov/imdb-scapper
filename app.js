9c8e86f79a74596cf92eda01cb0ccd6d183fd3a2
Doncho Minkov <DonchoMinkov@gmail.com>
2016-11-17 13:21:53 +0200

    Added multiple async requests

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
-/* globals console require */
+/* globals console require setTimeout Promise */
 "use strict";
 
-const listMoviesUrl = "http://www.imdb.com/search/title?genres=fantasy&title_type=feature&0sort=moviemeter,asc&page=2&view=simple&ref_=adv_nxt";
+const os = require("os");
 
-const genres = ["fantasy", "horror", "comedy", "action", "sci-fi"];
+const genres = ["action", "sci-fi", "fantasy", "horror", "comedy"];
+const pagesCount = 2;
 
 const httpRequester = require("./utils/http-requester");
 const htmlParser = require("./utils/html-parser");
 
-httpRequester.get(listMoviesUrl)
-    .then((result) => {
-        const selector = ".col-title span[title] a";
-        const html = result.body;
-        return htmlParser.parseSimpleMovie(selector, html);
-    })
-    .then(movies => {
-        console.log(movies);
-    })
-    .catch((err) => {
-        console.dir(err, { colors: true });
+const queuesFactory = require("./data-structures/queue");
+
+let urlsQueue = queuesFactory.getQueue();
+
+function wait(time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
     });
+}
+
+genres.forEach(genre => {
+    for (let i = 0; i < pagesCount; i += 1) {
+        let url = `http://www.imdb.com/search/title?genres=${genre}&title_type=feature&0sort=moviemeter,asc&page=${i+1}&view=simple&ref_=adv_nxt`;
+        urlsQueue.push(url);
+    }
+});
+
+function getMoviesFromUrl(url) {
+    console.log(`Working with ${url}`);
+    httpRequester.get(url)
+        .then((result) => {
+            const selector = ".col-title span[title] a";
+            const html = result.body;
+            return htmlParser.parseSimpleMovie(selector, html);
+        })
+        .then(movies => {
+            console.log(movies.length);
+            return wait(500);
+        })
+        .then(() => {
+            if (urlsQueue.isEmpty()) {
+                return;
+            }
+
+            getMoviesFromUrl(urlsQueue.pop());
+        })
+        .catch((err) => {
+            console.dir(err, { colors: true });
+        });
+}
+
+const asyncPagesCount = 5;
+
+Array.from({ length: asyncPagesCount })
+    .forEach(_ => getMoviesFromUrl(urlsQueue.pop()));
